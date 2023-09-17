277ac51b5ae70f38de23c6d8427f1c907755445f
Jorge Pardo <hi@jorgepardo.dev>
2023-09-17 23:10:44 +0200

    Initial Commit

---
+++ ConvertIt!/ConvertIt_App.swift
@@ -0,0 +1,17 @@
+//
+//  ConvertIt_App.swift
+//  ConvertIt!
+//
+//  Created by Jorge Pardo on 17/9/23.
+//
+
+import SwiftUI
+
+@main
+struct ConvertIt_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
